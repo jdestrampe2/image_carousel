9d7ccdd3776855e6ef44e6a8b56a263ea505ec70
Joseph C. Destrampe <gaiagreenworks@gmail.com>
2021-04-14 15:12:01 -0700

    Carousel Functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval = setInterval(run, 3000);
+
+function run() {
+  idx++
+  changeImg()
+}
+
+function changeImg() {
+  if(idx > img.length - 1) {
+    idx = 0
+  } else if(idx < 0) {
+    idx = img.length -1
+  }
+  imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInteval() {
+  clearInterval(interval)
+  interval = setInterval(run, 3000)
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++
+  changeImg()
+  resetInteval()
+})
+
+leftBtn.addEventListener('click', () => {
+  idx--
+  changeImg()
+  resetInteval()
+})
